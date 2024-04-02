b16ce1990db03e7757f705a481e68ba2e8eedcf8
Michael Walsh <walshmichael310@gmail.com>
2024-04-02 18:58:01 +0100

    CONFIG: Adding initial repo structure for backend

---
+++ backend/src/test/java/com/capstone/studentplacementplatform/StudentplacementplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.studentplacementplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentplacementplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
