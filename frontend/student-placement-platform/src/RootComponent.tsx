31821cbdbf7332d6ba7653a01735057fc48407ca
Kate Sweeney <katsweeney97@gmail.com>
2024-05-21 16:37:32 +0100

    fixing capitalisation errors

--- frontend/student-placement-platform/src/RootComponent.tsx
+++ frontend/student-placement-platform/src/RootComponent.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Homepage from './pages/Homepage'
+import Homepage from './pages/HomePage'
 import Login from './pages/Login'
 import NotFound from './pages/NotFound'
 import Register from './pages/Register'
