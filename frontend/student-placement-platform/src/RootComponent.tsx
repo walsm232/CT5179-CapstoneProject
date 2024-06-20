d1a3dd630f300ed84d0961abf53572fd613e2e81
Kate Sweeney <katsweeney97@gmail.com>
2024-06-20 18:46:28 +0100

    re added home page to root component

--- frontend/student-placement-platform/src/RootComponent.tsx
+++ frontend/student-placement-platform/src/RootComponent.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
-import InternshipDetail from './pages/InternshipDetail';
-import Login from './pages/Login';
-import NotFound from './pages/NotFound';
-import Register from './pages/Register';
-import StudentProfile from './pages/StudentProfile';
-import AddEducationHistory from './pages/AddEducationHistory';
-import EducationList from './pages/EducationList';
-import Account from './pages/Account';
-import StudentList from './pages/StudentList';
-import './styles/main.sass';
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Homepage from './pages/HomePage'
+import Dashboard from './pages/Dashboard'
+import InternshipDetail from './pages/InternshipDetail'
+import Login from './pages/Login'
+import NotFound from './pages/NotFound'
+import Register from './pages/Register'
+import StudentProfile from './pages/StudentProfile'
+import AddEducationHistory from './pages/AddEducationHistory'
+import EducationList from './pages/EducationList'
+import Account from './pages/Account'
+import StudentList from './pages/StudentList'
+import { ROUTES } from './resources/routes-constants'
+import './styles/main.sass'
 
 const RootComponent: React.FC = () => {
     return (
@@ -26,6 +28,7 @@ const RootComponent: React.FC = () => {
                 <Route path="dashboard" element={<Dashboard />} />
                 <Route path="account" element={<Account />} />
                 <Route path="internship/:id" element={<InternshipDetail />} />
+                <Route path={ROUTES.HOMEPAGE_ROUTE} element={<Homepage />} />
             </Routes>
         </Router>
     );
